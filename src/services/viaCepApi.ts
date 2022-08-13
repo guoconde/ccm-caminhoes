278d3e6efea761f4d6b45102df3136f306ae31a4
Gustavo <guoconde@pop-os.localdomain>
2022-08-13 19:17:33 -0300

    Implementing connection on viaCep

--- src/services/viaCepApi.ts
+++ src/services/viaCepApi.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import api from './api';
 
 export async function getAddressByCEP(value: string) {
-  const response = await axios.get(`http://viacep.com.br/ws/${value}/json/`);
+  const response = await api.post(`/viacep`, { cep: value });
 
   return response.data;
 }
